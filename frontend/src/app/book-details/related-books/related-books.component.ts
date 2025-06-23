a0623c7b016a36001b86f69e0684b67a76146a0b
K-nass <karimnase10@gmail.com>
2025-06-24 00:31:37 +0300

    Remove .angular cache from tracking and update .gitignore

---
+++ frontend/src/app/book-details/related-books/related-books.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-related-books',
+  standalone: true,
+  imports: [],
+  templateUrl: './related-books.component.html',
+  styleUrl: './related-books.component.css'
+})
+export class RelatedBooksComponent {
+
+}
--- frontend/src/app/wishlist/wishlist.component.ts
+++ frontend/src/app/wishlist/wishlist.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RatingComponent } from "../book-details/rating/rating.component";
 import { CartBtnComponent } from "../book-details/cart-btn/cart-btn.component";
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-wishlist',
   standalone: true,
-  imports: [RatingComponent, CartBtnComponent],
+  imports: [RatingComponent, CartBtnComponent,RouterLink],
   templateUrl: './wishlist.component.html',
   styleUrl: './wishlist.component.css'
 })
