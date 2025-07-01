d7e289cda4841e3e7624497d422c8793ff746ce4
K-nass <karimnase10@gmail.com>
2025-07-01 19:19:29 +0300

    added service in bookdetails for now to test and will be injected when admin works

---
+++ frontend/src/app/shared/services/socket.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Socket } from 'ngx-socket-io';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SocketService {
+  constructor(private socket: Socket) {}
+
+  onOrderCreated(): Observable<any> {
+    return this.socket.fromEvent('orderCreated');
+  }
+  
+}
