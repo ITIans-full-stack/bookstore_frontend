b0177b420300201b53343f059adab925adfa2af0
K-nass <karimnase10@gmail.com>
2025-07-03 05:48:56 +0300

    added option withCredentials to socket conf and and logs in socket service to check success of socket connection

--- frontend/src/app/shared/services/socket.service.ts
+++ frontend/src/app/shared/services/socket.service.ts
@@ -6,10 +6,19 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class SocketService {
-  constructor(private socket: Socket) {}
+  // constructor(private socket: Socket) {}
+  constructor(private socket: Socket) {
+    this.socket.on('connect', () => {
+      console.log('✅ Socket connected:', this.socket.ioSocket.id);
+    });
+
+    this.socket.on('connect_error', (err:any) => {
+      console.error('❌ Socket connection error:', err);
+    });
+  }
 
   onOrderCreated(): Observable<any> {
     return this.socket.fromEvent('orderCreated');
   }
-  
+
 }
