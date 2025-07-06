10dd9debea426e934f0f497ef18c7ad988559e2b
K-nass <karimnase10@gmail.com>
2025-07-06 21:46:24 +0300

    added washlist service and washlist component

--- frontend/src/app/shared/components/wishlist-btn/wishlist-btn.component.ts
+++ frontend/src/app/shared/components/wishlist-btn/wishlist-btn.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
-
+import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { WishlistService } from '../../services/wishlist.service';
+import { BookInterface } from '../../../core/interfaces/book-interface';
 
 @Component({
   selector: 'app-wishlist-btn',
   standalone: true,
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './wishlist-btn.component.html',
-  styleUrl: './wishlist-btn.component.css'
+  styleUrl: './wishlist-btn.component.css',
 })
 export class WishlistBtnComponent {
-  
+  @Input() book!: BookInterface;
+
+  constructor(
+    private wishlistService: WishlistService,
+    private router: Router
+  ) { }
+
+  addToWishlist(): void {
+    this.wishlistService.addToWishlist(this.book);
+    this.router.navigate(['/wishlist']);
+  }
 }
--- frontend/src/app/wishlist/wishlist.component.ts
+++ frontend/src/app/wishlist/wishlist.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BookCardComponent } from '../shared/components/book-card/book-card.component';
+import { BookInterface } from '../core/interfaces/book-interface';
+import { WishlistService } from '../shared/services/wishlist.service';
 
 
 @Component({
   selector: 'app-wishlist',
   standalone: true,
-  imports: [ BookCardComponent],
+  imports: [BookCardComponent],
   templateUrl: './wishlist.component.html',
   styleUrl: './wishlist.component.css'
 })
-export class WishlistComponent {
+export class WishlistComponent implements OnInit {
+  wishlistBooks: BookInterface[] = [];
 
+  constructor(private wishlistService: WishlistService) { }
+
+  ngOnInit(): void {
+    this.wishlistService.wishlist$.subscribe((books) => {
+      this.wishlistBooks = books;
+    });
+  }
+
+  remove(bookId: string) {
+    this.wishlistService.removeFromWishlist(bookId);
+  }
 }
