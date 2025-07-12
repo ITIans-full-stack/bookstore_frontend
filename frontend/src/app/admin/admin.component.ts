a9f88fd3ef0e18167dd16981df1a5f93fff17c5f
K-nass <karimnase10@gmail.com>
2025-07-12 13:26:56 +0300

    added live ordres to view

--- frontend/src/app/admin/admin.component.ts
+++ frontend/src/app/admin/admin.component.ts
@@ -1,25 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { RouterModule } from '@angular/router';
 import { Router } from '@angular/router';
 
 import { AuthService } from '../shared/services/auth.service';
+import { SocketService } from '../shared/services/socket.service';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.css']
 })
-export class AdminComponent {
+export class AdminComponent implements OnInit {
+  orders: any[] = [];
 
+  constructor(private router: Router, private authService: AuthService, private socketService: SocketService) { }
 
-  constructor(private router: Router, private authService: AuthService) {}
+  ngOnInit() {
+    this.socketService.onOrderCreated().subscribe((order) => {
+      console.log('New Order Received in Admin Panel:', order);
+      this.orders.unshift(order);
+    });
+  }
 
   logout() {
     this.authService.logout();
-    this.router.navigate(['/login'],{ replaceUrl: true });
+    this.router.navigate(['/login'], { replaceUrl: true });
   }
 
 
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from "./nav-bar/nav-bar.component";
 import { FooterComponent } from './footer/footer.component';
+import { SocketService } from './shared/services/socket.service';
+
 
 @Component({
   selector: 'app-root',
